017ecb427d3c2c8dd8fb0eebfbc1bf8406558b98
@shimura.kosuke.infratop <shimura.kosuke.infratop@gmail.com>
2021-08-09 13:04:56 +0000

    showページのいいね非同期通信成功

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
     @show_book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite = current_user.favorites.new(book_id: @show_book.id)
     favorite.save
   end
   
   def destroy
     @show_book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite = current_user.favorites.find_by(book_id: @show_book.id)
     favorite.destroy
   end
 end
